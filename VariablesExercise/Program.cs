a1f0bf8708e2ca9fdb51507b4ececa4483c2ecaf
Marcus Johnson <marx5185@gmail.com>
2025-06-18 00:59:34 -0500

    created a story rpg thing that includes variables i have learned

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string catsName = "Casper";
+
+            int catsAge = 3;
+
+            char catsGender = 'M';
+
+            bool catIsFed = true;
+
+            double catsWeight = 100;
+
+            decimal catsMoney = 0.00m;
+            
+            Console.WriteLine("Cat destroyer 9000 activated");
+            Console.WriteLine("Finding nearest cat...");
+            Console.WriteLine ($"Cat found, name is {catsName}, age is {catsAge}, gender is {catsGender}");
+            Console.WriteLine("advancing towards cat");
+            Console.WriteLine($"oh no it looks like this cat weighs at least {catsWeight} lb");
+            Console.WriteLine("Cat hit me... going down...");
+            Console.WriteLine("");
+            Console.WriteLine($"Cat has gained 30 xp and ${catsMoney}");
         }
     }
 }
